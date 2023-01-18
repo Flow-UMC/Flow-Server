d1b369f1f5cd7a0888a7bc7ce074fdf3338ea4c2
dawoon08 <lusia08291@gmail.com>
2023-01-18 15:15:55 +0900

    feat: 캘린더 API 구현

---
+++ src/main/java/com/flow/flow/src/calendar/CalendarProvider.java
@@ -0,0 +1,55 @@
+package com.flow.flow.src.calendar;
+
+import com.flow.flow.config.BaseException;
+import com.flow.flow.src.calendar.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.flow.flow.config.BaseResponseStatus.DATABASE_ERROR;
+
+@Service
+public class CalendarProvider {
+
+    private final CalendarDao calendarDao;
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public CalendarProvider(CalendarDao calendarDao) {
+        this.calendarDao = calendarDao;
+    }
+
+
+    public List<GetCalendarRes> getCalendar(int year, int month, int userId) throws BaseException {
+        try{
+            List<GetCalendarRes> getCalendarRes = calendarDao.getCalendar(year, month, userId);
+            return getCalendarRes;
+        } catch(Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    public GetTranByDateRes getTranByDate(int year, int month, int date, int userId) throws BaseException{
+        try{
+            List<Transaction> transaction = calendarDao.getTransaction(year, month, date, userId);
+            List<GetTotalAmount> getTotalAmount = calendarDao.getTotalAmount(year, month, date, userId);
+            GetTranByDateRes getTranByDateRes = new GetTranByDateRes(getTotalAmount, transaction) ;
+            return getTranByDateRes;
+        } catch(Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+    public List<Transaction> getTransaction(int year, int month, int date, int userId) throws BaseException{
+        try{
+            List<Transaction> transaction = calendarDao.getTransaction(year, month, date, userId);
+            return transaction;
+        } catch(Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+}
+
+
---
+++ src/main/java/com/flow/flow/src/calendar/model/Transaction.java
@@ -0,0 +1,19 @@
+package com.flow.flow.src.calendar.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Transaction {
+    private int isExp;
+    private String info;
+    private int amount;
+    private Time time;
+    private String memo;
+}
+
