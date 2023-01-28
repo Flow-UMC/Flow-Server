e2e185aa05163051fee4789382e87a5348b12def
cyjadela <adelacho@naver.com>
2023-01-28 17:29:52 +0900

    feat: CategoryProvider(카테고리 조회)

--- src/main/java/com/flow/category/CategoryProvider.java
+++ src/main/java/com/flow/category/CategoryProvider.java
@@ -1,7 +1,13 @@
 package com.flow.category;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.flow.config.BaseException;
+import com.flow.config.BaseResponseStatus;
+import com.flow.model.GetCategoryRes;
 
 @Service
 public class CategoryProvider {
@@ -11,6 +17,16 @@ public class CategoryProvider {
     @Autowired
     public CategoryProvider(CategoryDao categoryDao) {
         this.categoryDao = categoryDao;
+    }
+
+    //카테고리 목록 조회
+    public List<GetCategoryRes> getCategorys(int userId) throws BaseException {
+        try {
+            List<GetCategoryRes> getCategoryRes = categoryDao.getCategorys(userId);
+            return getCategoryRes;
+        } catch (Exception exception) {
+            throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
+        }
     }
 
 }
