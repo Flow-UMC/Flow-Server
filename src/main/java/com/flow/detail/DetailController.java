ff9bc6dad1a400d6f190e864c0a3d6f33f3bd8e3
choiyounji <yunji71@naver.com>
2023-02-04 17:06:22 +0900

    add: logger 추가

--- src/main/java/com/flow/detail/DetailController.java
+++ src/main/java/com/flow/detail/DetailController.java
@@ -3,6 +3,8 @@ package com.flow.detail;
 import com.flow.model.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.flow.config.BaseException;
 import com.flow.config.BaseResponse;
 import static com.flow.config.BaseResponseStatus.*;
@@ -11,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/details")
 public class DetailController {
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
     
     @Autowired
     private final DetailProvider detailProvider;
--- src/main/java/com/flow/detail/DetailProvider.java
+++ src/main/java/com/flow/detail/DetailProvider.java
@@ -3,10 +3,15 @@ package com.flow.detail;
 import com.flow.model.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DetailProvider {
+
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     DetailDao detailDao;
 
