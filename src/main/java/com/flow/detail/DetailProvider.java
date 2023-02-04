a65b749c28b035e697ea30da805cf012fd51cd73
cyjadela <adelacho@naver.com>
2023-02-04 14:20:37 +0900

    feat: 상세 내역 조회 provider 구현

--- src/main/java/com/flow/detail/DetailProvider.java
+++ src/main/java/com/flow/detail/DetailProvider.java
@@ -1,5 +1,7 @@
 package com.flow.detail;
 
+import com.flow.config.BaseException;
+import com.flow.config.BaseResponseStatus;
 import com.flow.model.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +14,15 @@ public class DetailProvider {
 
     public List<Detail> getDetails(Pagination pagination, int userId, String year, String month){
         return detailDao.getDetails(pagination, userId, year, month);
+    }
+
+     //상세 내역 조회
+     public GetDetailRes getDetail(int userId, int detailId) throws BaseException {
+        try {
+            GetDetailRes getDetailRes = detailDao.getDetail(userId, detailId);
+            return getDetailRes;
+        } catch (Exception exception) {
+            throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
+        }
     }
 }
