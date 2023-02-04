7efac970dc5b4367f8dbcb72ea1133bc8f4b5b6b
cyjadela <adelacho@naver.com>
2023-02-04 14:31:32 +0900

    feat: 상세 내역 수정 service 구현

--- src/main/java/com/flow/detail/DetailService.java
+++ src/main/java/com/flow/detail/DetailService.java
@@ -1,5 +1,7 @@
 package com.flow.detail;
 
+import com.flow.config.BaseException;
+import com.flow.config.BaseResponseStatus;
 import com.flow.model.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +23,16 @@ public class DetailService {
     public void deleteDetail(GetDeleteDetailRes detailIds){
         detailDao.deleteDetail(detailIds);
     }
+
+        //상세 내역 수정(Patch)
+        public void modifyDetail(int userId, int detailId, PatchDetailReq patchDetailReq) throws BaseException {
+            try {
+                int result = detailDao.modifyDetail(userId, detailId, patchDetailReq);
+                if (result == 0) {
+                    throw new BaseException(BaseResponseStatus.MODIFY_FAIL_DETAIL);
+                }
+            } catch (Exception exception) {
+                throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
+            }
+        }
 }
